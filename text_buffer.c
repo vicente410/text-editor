b3f8706f5bb376ddfeff7093a8dadd788f2068b0
vicente410 <brunottvicente@gmail.com>
2025-08-28 18:34:08 +0000

    Expanded text buffer interface

--- text_buffer.c
+++ text_buffer.c
@@ -1,6 +1,4 @@
 #include "text_buffer.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 struct TextBuffer {
     char *data;
@@ -25,14 +23,73 @@ void tb_debug(TextBuffer *tb)
     printf("%s", tb->data);
 }
 
-void tb_read_from_file(TextBuffer *tb, char *path)
+void tb_load_file(TextBuffer *tb, char *path)
 {
     fprintf(stderr, "TODO: implement tb_read_from_file");
     exit(1);
 }
 
-void tb_write_to_file(TextBuffer *tb, char *path)
+void tb_save_file(TextBuffer *tb, char *path)
 {
     fprintf(stderr, "TODO: implement tb_write_to_file");
     exit(1);
 }
+
+size_t tb_length(TextBuffer *tb)
+{
+    fprintf(stderr, "TODO: implement tb_length");
+    exit(1);
+}
+
+void tb_insert(TextBuffer *tb, size_t idx, char *str)
+{
+    fprintf(stderr, "TODO: implement tb_insert");
+    exit(1);
+}
+
+void tb_insert_ch(TextBuffer *tb, size_t idx, char ch)
+{
+    fprintf(stderr, "TODO: implement tb_insert_ch");
+    exit(1);
+}
+
+void tb_remove(TextBuffer *tb, size_t start, size_t end)
+{
+    fprintf(stderr, "TODO: implement tb_remove");
+    exit(1);
+}
+
+void tb_remove_ch(TextBuffer *tb, size_t idx)
+{
+    fprintf(stderr, "TODO: implement tb_remove_ch");
+    exit(1);
+}
+
+size_t tb_find(TextBuffer *tb, char *str)
+{
+    fprintf(stderr, "TODO: implement tb_find");
+    exit(1);
+	return 0;
+}
+
+size_t tb_findr(TextBuffer *tb, char *str)
+{
+    fprintf(stderr, "TODO: implement tb_findr");
+    exit(1);
+	return 0;
+}
+
+size_t tb_find_ch(TextBuffer *tb, char ch)
+{
+    fprintf(stderr, "TODO: implement tb_find_ch");
+    exit(1);
+	return 0;
+}
+
+size_t tb_findr_ch(TextBuffer *tb, char ch)
+{
+    fprintf(stderr, "TODO: implement tb_findr_ch");
+    exit(1);
+	return 0;
+}
+
--- text_buffer.h
+++ text_buffer.h
@@ -1,7 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct TextBuffer TextBuffer;
 
 TextBuffer *tb_new();
 void tb_free(TextBuffer *tb);
 void tb_debug(TextBuffer *tb);
-void tb_read_from_file(TextBuffer *tb, char *path);
-void tb_write_to_file(TextBuffer *tb, char *path);
+
+void tb_load_file(TextBuffer *tb, char *path);
+void tb_save_file(TextBuffer *tb, char *path);
+
+size_t tb_length(TextBuffer *tb);
+void tb_insert(TextBuffer *tb, size_t idx, char *str);
+void tb_insert_ch(TextBuffer *tb, size_t idx, char ch);
+void tb_remove(TextBuffer *tb, size_t start, size_t end);
+void tb_remove_ch(TextBuffer *tb, size_t idx);
+
+size_t tb_find(TextBuffer *tb, char *str);
+size_t tb_findr(TextBuffer *tb, char *str);
+size_t tb_find_ch(TextBuffer *tb, char ch);
+size_t tb_findr_ch(TextBuffer *tb, char ch);
